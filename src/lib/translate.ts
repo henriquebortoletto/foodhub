4d83f7edef2e029d344b21d4f7cb89f7d002cd7c
Henrique Bortoletto <bortolettohenrique@gmail.com>
2025-03-07 18:18:06 -0300

    add translate login response firebase

--- src/lib/translate.ts
+++ src/lib/translate.ts
@@ -1,5 +1,6 @@
 const translate: Record<string, string> = {
   'auth/email-already-in-use': 'Esse email já está em uso.',
+  'auth/invalid-credential': 'Email ou senha inválidos.',
 }
 
 type Translate = (key: string) => string
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -3,12 +3,13 @@ import { useNavigate } from 'react-router'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { ChefHat } from 'lucide-react'
+import { FirebaseError } from 'firebase/app'
 import * as z from 'zod'
 
 import { TextField, Typography } from '@mui/material'
-import { loginWithEmail } from '@/services/authService'
-
 import { Card } from '@/components/Card'
+import { loginWithEmail } from '@/services/authService'
+import { transformTranslate } from '@/lib/translate'
 
 import * as S from './styles'
 
@@ -41,7 +42,13 @@ export const Login = () => {
       await loginWithEmail({ email, password })
       navigate('/')
     } catch (error) {
-      console.error('Erro ao autenticar:', error)
+      if (error instanceof FirebaseError) {
+        console.error('Erro de autenticação:', error.code)
+        console.log(error.code)
+        alert(transformTranslate(error.code))
+      } else {
+        console.error('Erro desconhecido:', error)
+      }
     } finally {
       setLoading(false)
     }
